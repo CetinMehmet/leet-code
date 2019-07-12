111c893a79a9cbd45bd3682254777b3dfbe78ed5
Mehmet Cetin <43109419+CetinMehmet@users.noreply.github.com>
2019-07-12 19:45:39 +0200

    Update max-consecutive-ones.cpp

--- max-consecutive-ones.cpp
+++ max-consecutive-ones.cpp
@@ -25,3 +25,30 @@ public:
 
 
 // O(n) solution
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int maxLen = 0;
+        int i = 0;
+        while(i < nums.size()) {
+            if (nums[i] == 1) {
+                int curr_len = 1;
+                int j = i + 1;
+                while(j < nums.size() && nums[j] == 1) {
+                    curr_len++;
+                    j++;
+                }
+                
+                // Very important line
+                // Prevents us from checking the array again after finding the longest 1s
+                i = j;
+                
+                maxLen = max(maxLen, curr_len);
+            }
+            else {
+                i++;
+            }
+        }
+        return maxLen;
+    }
+};
